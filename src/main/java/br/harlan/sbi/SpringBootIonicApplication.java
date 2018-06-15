55f85d330ab59feb3ddcdc7607329b25ab46d7da
Harlan Cleiton <harlancleiton@gmail.com>
2018-06-14 23:23:47 -0300

    Implementation of CommandLineRunner in order to instantiate two categories at the beginning of the application.

--- src/main/java/br/harlan/sbi/SpringBootIonicApplication.java
+++ src/main/java/br/harlan/sbi/SpringBootIonicApplication.java
@@ -1,12 +1,27 @@
 package br.harlan.sbi;
 
+import br.harlan.sbi.entities.Category;
+import br.harlan.sbi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootIonicApplication {
+public class SpringBootIonicApplication implements CommandLineRunner {
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIonicApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category category = new Category("Informatica");
+        Category category1 = new Category("Escritorio");
+        categoryRepository.saveAll(Arrays.asList(category, category1));
     }
 }
