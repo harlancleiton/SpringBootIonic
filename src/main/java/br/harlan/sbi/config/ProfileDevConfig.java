dd2c92a1f753dbee873d999331ad68d9c8308223
Harlan Cleiton <harlancleiton@gmail.com>
2018-06-21 10:40:09 -0300

    Prod Profile. Heroku integration.

--- src/main/java/br/harlan/sbi/config/ProfileDevConfig.java
+++ src/main/java/br/harlan/sbi/config/ProfileDevConfig.java
@@ -2,6 +2,7 @@ package br.harlan.sbi.config;
 
 import br.harlan.sbi.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,9 +13,13 @@ public class ProfileDevConfig {
     @Autowired
     DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategyDdlAuto;
+
     @Bean
     public boolean instantiateDatabase() throws Exception {
-        dbService.instantiateTestDatabase();
+        if (strategyDdlAuto.equals("create"))
+            dbService.instantiateTestDatabase();
         return true;
     }
 }
